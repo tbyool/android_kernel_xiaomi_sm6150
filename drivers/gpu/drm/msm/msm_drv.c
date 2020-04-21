edbc5e22954edef618e94dfbb00070cf9264ea5e
Peter Zijlstra <peterz@infradead.org>
2020-04-21 12:09:13 +0200

    sched,msm: Convert to sched_set_fifo*()
    Because SCHED_FIFO is a broken scheduler model (see previous patches)
    take away the priority field, the kernel can't possibly make an
    informed decision.
    
    Use sched_set_fifo(); Effectively changes prio from 16 to 50.
    
    Cc: airlied@redhat.com
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -88,7 +88,6 @@ static void msm_fb_output_poll_changed(struct drm_device *dev)
 static void msm_drm_display_thread_priority_worker(struct kthread_work *work)
 {
 	int ret = 0;
-	struct sched_param param = { 0 };
 	struct task_struct *task = current->group_leader;
 
 	/**
@@ -96,8 +95,7 @@ static void msm_drm_display_thread_priority_worker(struct kthread_work *work)
 	 * realtime scheduling to process display updates and interact with
 	 * other real time and normal priority task
 	 */
-	param.sched_priority = 16;
-	ret = sched_setscheduler(task, SCHED_FIFO, &param);
+	ret = sched_set_fifo(task);
 	if (ret)
 		pr_warn("pid:%d name:%s priority update failed: %d\n",
 			current->tgid, task->comm, ret);
