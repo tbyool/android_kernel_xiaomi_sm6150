cfd6b7f36f605d7a6150a5c0de36b698cb8f377b
Peter Zijlstra <peterz@infradead.org>
2020-04-22 13:10:04 +0200

    sched: Remove sched_set_*() return value
    Ingo suggested that since the new sched_set_*() functions are
    implemented using the 'nocheck' variants, they really shouldn't ever
    fail, so remove the return value.
    
    Cc: axboe@kernel.dk
    Cc: daniel.lezcano@linaro.org
    Cc: sudeep.holla@arm.com
    Cc: airlied@redhat.com
    Cc: broonie@kernel.org
    Cc: paulmck@kernel.org
    Suggested-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -87,7 +87,6 @@ static void msm_fb_output_poll_changed(struct drm_device *dev)
 
 static void msm_drm_display_thread_priority_worker(struct kthread_work *work)
 {
-	int ret = 0;
 	struct task_struct *task = current->group_leader;
 
 	/**
@@ -95,10 +94,7 @@ static void msm_drm_display_thread_priority_worker(struct kthread_work *work)
 	 * realtime scheduling to process display updates and interact with
 	 * other real time and normal priority task
 	 */
-	ret = sched_set_fifo(task);
-	if (ret)
-		pr_warn("pid:%d name:%s priority update failed: %d\n",
-			current->tgid, task->comm, ret);
+	sched_set_fifo(task);
 }
 
 /**
