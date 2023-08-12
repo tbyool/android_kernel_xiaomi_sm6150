0580cb74c7471a9519c8dfad2ad09126b9c85b62
AkumaHunt3r@protonmail.com <AkumaHunt3r@protonmail.com>
2023-08-12 20:33:39 +0000

    Revert "drm: Boost DDR bus upon running an atomic ioctl"
    This reverts commit 618a8bbdddfa6963069edc6634e40c6276056c1c.
    
    Honestly, no visible improvements. To avoid possible battery usage due to very constant boosting, revert
    
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -30,8 +30,6 @@
 #include <drm/drm_atomic.h>
 #include <drm/drm_mode.h>
 #include <drm/drm_print.h>
-#include <linux/cpu_input_boost.h>
-#include <linux/devfreq_boost.h>
 #include <linux/pm_qos.h>
 #include <linux/sync_file.h>
 
@@ -2261,14 +2259,6 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 	if ((arg->flags & DRM_MODE_ATOMIC_TEST_ONLY) &&
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
-
-	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
-		if (cpu_input_boost_within_input(3250))
-			cpu_input_boost_kick();
-
-		if (df_boost_within_input(3250))
-			devfreq_boost_kick(DEVFREQ_CPU_LLCC_DDR_BW);
-	}
 
 	drm_modeset_acquire_init(&ctx, 0);
 
