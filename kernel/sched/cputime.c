b942dd99a2e5525e39c70e37c56aabe3afdf45a4
Yafang Shao <laoar.shao@gmail.com>
2025-01-03 10:24:06 +0800

    sched: Define sched_clock_irqtime as static key
    Since CPU time accounting is a performance-critical path, let's define
    sched_clock_irqtime as a static key to minimize potential overhead.
    
    Signed-off-by: Yafang Shao <laoar.shao@gmail.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Michal Koutný <mkoutny@suse.com>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lore.kernel.org/r/20250103022409.2544-2-laoar.shao@gmail.com
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- kernel/sched/cputime.c
+++ kernel/sched/cputime.c
@@ -10,6 +10,8 @@
 #include "walt.h"
 
 #ifdef CONFIG_IRQ_TIME_ACCOUNTING
+DEFINE_STATIC_KEY_FALSE(sched_clock_irqtime);
+
 
 /*
  * There are no locks covering percpu hardirq/softirq time.
@@ -24,16 +26,14 @@
  */
 DEFINE_PER_CPU(struct irqtime, cpu_irqtime);
 
-static int sched_clock_irqtime;
-
 void enable_sched_clock_irqtime(void)
 {
-	sched_clock_irqtime = 1;
+	static_branch_enable(&sched_clock_irqtime);
 }
 
 void disable_sched_clock_irqtime(void)
 {
-	sched_clock_irqtime = 0;
+	static_branch_disable(&sched_clock_irqtime);
 }
 
 static void irqtime_account_delta(struct irqtime *irqtime, u64 delta,
@@ -62,7 +62,7 @@ void irqtime_account_irq(struct task_struct *curr)
 	bool account = true;
 #endif
 
-	if (!sched_clock_irqtime)
+	if (!irqtime_enabled())
 		return;
 
 	cpu = smp_processor_id();
@@ -106,8 +106,6 @@ static u64 irqtime_tick_accounted(u64 maxtime)
 }
 
 #else /* CONFIG_IRQ_TIME_ACCOUNTING */
-
-#define sched_clock_irqtime	(0)
 
 static u64 irqtime_tick_accounted(u64 dummy)
 {
@@ -500,7 +498,7 @@ void account_process_tick(struct task_struct *p, int user_tick)
 	if (vtime_accounting_cpu_enabled())
 		return;
 
-	if (sched_clock_irqtime) {
+	if (irqtime_enabled()) {
 		irqtime_account_process_tick(p, user_tick, rq, 1);
 		return;
 	}
@@ -529,7 +527,7 @@ void account_idle_ticks(unsigned long ticks)
 {
 	u64 cputime, steal;
 
-	if (sched_clock_irqtime) {
+	if (irqtime_enabled()) {
 		irqtime_account_idle_ticks(ticks);
 		return;
 	}
