b243aafabfff824e80fcac483840da9fcd6d882e
Vincent Guittot <vincent.guittot@linaro.org>
2026-04-22 11:34:00 +0200

    sched/fair: Fix wakeup_preempt_fair() vs delayed dequeue
    Similar to how pick_next_entity() must dequeue delayed entities, so too must
    wakeup_preempt_fair(). Any delayed task being found means it is eligible and
    hence past the 0-lag point, ready for removal.
    
    Worse, by not removing delayed entities from consideration, it can skew the
    preemption decision, with the end result that a short slice wakeup will not
    result in a preemption.
    
                         tip/sched/core  tip/sched/core    +this patch
    cyclictest slice  (ms) (default)2.8             8               8
    hackbench slice   (ms) (default)2.8            20              20
    Total Samples          |    22559           22595           22683
    Average           (us) |      157              64( 59%)        59(  8%)
    Median (P50)      (us) |       57              57(  0%)        58(- 2%)
    90th Percentile   (us) |       64              60(  6%)        60(  0%)
    99th Percentile   (us) |     2407              67( 97%)        67(  0%)
    99.9th Percentile (us) |     3400            2288( 33%)       727( 68%)
    Maximum           (us) |     5037            9252(-84%)      7461( 19%)
    
    Fixes: f12e148892ed ("sched/fair: Prepare pick_next_task() for delayed dequeue")
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://patch.msgid.link/20260422093400.319251-1-vincent.guittot@linaro.org
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1158,7 +1158,7 @@ static inline void cancel_protect_slice(struct sched_entity *se)
  *
  * Which allows tree pruning through eligibility.
  */
-static struct sched_entity *__pick_eevdf(struct cfs_rq *cfs_rq, bool protect)
+static struct sched_entity *pick_eevdf(struct cfs_rq *cfs_rq, bool protect)
 {
 	struct rb_node *node = cfs_rq->tasks_timeline.rb_root.rb_node;
 	struct sched_entity *se = __pick_first_entity(cfs_rq);
@@ -1229,11 +1229,6 @@ found:
 		best = curr;
 
 	return best;
-}
-
-static struct sched_entity *pick_eevdf(struct cfs_rq *cfs_rq)
-{
-        return __pick_eevdf(cfs_rq, true);
 }
 
 #ifdef CONFIG_SCHED_DEBUG
@@ -5029,9 +5024,9 @@ static int dequeue_entities(struct rq *rq, struct sched_entity *se, int flags);
  * 4) do not run the "skip" process, if something else is available
  */
 static struct sched_entity *
-pick_next_entity(struct rq *rq, struct cfs_rq *cfs_rq)
+pick_next_entity(struct rq *rq, struct cfs_rq *cfs_rq, bool protect)
 {
-	struct sched_entity *se = pick_eevdf(cfs_rq);
+	struct sched_entity *se = pick_eevdf(cfs_rq, protect);
 
 	/*
 	 * Picking the ->next buddy will affect latency but not fairness.
@@ -9389,7 +9384,7 @@ static void check_preempt_wakeup(struct rq *rq, struct task_struct *p, int wake_
 {
 	enum preempt_wakeup_action preempt_action = PREEMPT_WAKEUP_PICK;
 	struct task_struct *curr = rq->curr;
-	struct sched_entity *se = &curr->se, *pse = &p->se;
+	struct sched_entity *nse, *se = &curr->se, *pse = &p->se;
 	struct cfs_rq *cfs_rq = task_cfs_rq(curr);
 	bool do_preempt_short = false;
 
@@ -9476,11 +9471,18 @@ static void check_preempt_wakeup(struct rq *rq, struct task_struct *p, int wake_
 	 */
 	do_preempt_short = sched_feat(PREEMPT_SHORT) && (pse->slice < se->slice);
 
-	/*
-	 * If @p has become the most eligible task, force preemption.
-	 */
-	if (__pick_eevdf(cfs_rq, !do_preempt_short) == pse)
+pick:
+	nse = pick_next_entity(rq, cfs_rq, !do_preempt_short);
+	/* If @p has become the most eligible task, force preemption */
+	if (nse == pse)
 		goto preempt;
+
+	/*
+	 * Because p is enqueued, nse being null can only mean that we
+	 * dequeued a delayed task.
+	 */
+	if (!nse)
+		goto pick;
 
 	if (sched_feat(RUN_TO_PARITY) && do_preempt_short)
 		update_protect_slice(cfs_rq, se);
@@ -9514,7 +9516,7 @@ again:
 		if (unlikely(check_cfs_rq_runtime(cfs_rq)))
 			goto again;
 
-		se = pick_next_entity(rq, cfs_rq);
+		se = pick_next_entity(rq, cfs_rq, true);
 		if (!se)
 			goto again;
 		cfs_rq = group_cfs_rq(se);
