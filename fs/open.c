7626f3cee3ba7f0207f2178eac7751ed65abde32
Richard Raya <rdxzv.dev@gmail.com>
2026-08-06 16:42:50 -0300

    af_unix: Block all powerhal services from writing to logd
    perf@2.2, perf-hal, perf2-hal, iop and the lineage/xiaomi libperfmgr
    services all spam logd when the kernel lacks the knobs they expect.
    Extend the perfd tracking from the af_unix logd blocking commit
    (f696289f34e3) to cover them all under one powerhal_tsk, and block
    their logdw socket writes.
    
    Change-Id: I05d1f148cfaaf52ff30fa01f99222552cb0baf66
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- fs/open.c
+++ fs/open.c
@@ -1053,7 +1053,7 @@ struct file *file_open_root(struct dentry *dentry, struct vfsmount *mnt,
 }
 EXPORT_SYMBOL(file_open_root);
 
-bool task_is_perfd(struct task_struct *p);
+bool task_is_powerhal(struct task_struct *p);
 static bool libperfmgr_redirect(struct file **f, int dfd, struct filename *n,
 				struct open_flags *op, int flags)
 {
@@ -1076,7 +1076,7 @@ static bool libperfmgr_redirect(struct file **f, int dfd, struct filename *n,
 	if (likely(*f != ERR_PTR(-ENOENT) ||
 	    (flags & REQUIRED_FLAGS) != REQUIRED_FLAGS ||
 	    flags & ~ALLOWED_FLAGS ||
-	    !task_is_perfd(current)))
+	    !task_is_powerhal(current)))
 		return false;
 #undef ALLOWED_FLAGS
 #undef REQUIRED_FLAGS
