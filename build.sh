3350990e1e52d0f47d7f0ab451b657ed44ac8808
Yahya Wessam <yahyawessam2002@gmail.com>
2026-08-09 05:26:12 +0300

    build.sh: revamp
    Support to download Neutron clang instead of aospclang, do note that now -ac or -nc will need to be passed
    Removed GCC compilation dead code
    Bump AK3 support versions to A17
    
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@
 # Initialize flags for options
 clean=false
 clang=true
-gcc=flase
+aclang=false
+nclang=false
 
 # Use getopt for parsing long and short options
 while [[ $# -gt 0 ]]; do
@@ -15,12 +16,12 @@ while [[ $# -gt 0 ]]; do
       clean=true
       shift
       ;;
-    -cc|--clang)
-      clang=true
+    -ac|--aospclang)
+      aclang=true
       shift
       ;;
-    -gc|--gcc)
-      gcc=true
+    -nc|--neutronclang)
+      nclang=true
       shift
       ;;
     *)
@@ -39,14 +40,28 @@ export KBUILD_BUILD_HOST=tbyool
 export ARCH=arm64
 
 if [ "$clang" = true ]; then
-	gcc=false
-	echo -e "\nCompiling with Clang!\n"
-	CLANG_URL=$(curl -s https://api.github.com/repos/bachnxuan/aosp_clang_mirror/releases/latest | grep "browser_download_url" | head -n 1 | cut -d '"' -f 4)
-	if [ ! -d "$PWD/clang" ]; then
-		curl -L -O "$CLANG_URL"
-		tar -C clang -xf clang-*.tar.gz
-	else
-		echo "Local clang dir found, will not download clang and using that instead"
+	if [ "$aclang" = true ]; then
+		echo -e "\nCompiling with AOSP Clang!\n"
+		CLANG_URL=$(curl -s https://api.github.com/repos/bachnxuan/aosp_clang_mirror/releases/latest | grep "browser_download_url" | head -n 1 | cut -d '"' -f 4)
+		if [ ! -d "$PWD/clang" ]; then
+			mkdir clang
+			curl -L -O "$CLANG_URL"
+			tar -C clang -xf clang-*.tar.gz
+		else
+			echo "Local clang dir found, will not download clang and using that instead"
+		fi
+	fi
+
+	if [ "$nclang" = true ]; then
+		echo -e "\nCompiling with Neutron Clang!\n"
+		CLANG_URL=$(curl -s https://api.github.com/repos/Neutron-Toolchains/clang-build-catalogue/releases/latest | grep "browser_download_url" | head -n 1 | cut -d '"' -f 4)
+		if [ ! -d "$PWD/clang" ]; then
+			mkdir clang
+			curl -L -O "$CLANG_URL"
+			tar -C clang -xf neutron-clang-*.tar.zst
+		else
+			echo "Local clang dir found, will not download clang and using that instead"
+		fi
 	fi
 
 	$PWD/clang/bin/clang --version | head -n1
@@ -60,46 +75,6 @@ if [ "$clang" = true ]; then
 	export LLVM_IAS=1
 fi
 
-if [ "$gcc" = true ]; then
-	clang=false
-	echo -e "\nCompiling with GCC!\n"
-	GCC_URLS=$(curl -s "https://api.github.com/repos/mvaisakh/gcc-build/releases/latest" | grep "browser_download_url" | cut -d '"' -f 4 | grep -E "eva-gcc-arm.*\.xz")
-	if [ ! -d "$PWD/gcc32" ] && [ ! -d "$PWD/gcc64" ]; then
-		for url in $GCC_URLS; do
-			curl -L -O "$url"
-		done
-		for file in eva-gcc-arm*.xz; do
-			#The files are actually just plain tarballs named as .xz, do not call xz to decompress
-			if [[ "$file" == *arm64* ]]; then
-				tar -xf "$file" && mv gcc-arm64 gcc64
-			else
-				tar -xf "$file" && mv gcc-arm gcc32
-			fi
-			rm -rf "$file"
-		done
-	else
-		echo "Local gcc dirs found, will not download gcc and using those instead"
-	fi
-
-	export GCC64_DIR=$PWD/gcc64
-	$GCC64_DIR/bin/aarch64-elf-gcc --version | head -n1
-	export GCC32_DIR=$PWD/gcc32
-	export KBUILD_COMPILER_STRING="$("$GCC64_DIR/bin/aarch64-elf-gcc" --version | head -n1)"
-	export PATH="$GCC64_DIR/bin:$GCC32_DIR/bin:$PATH"
-	export CROSS_COMPILE="aarch64-elf-"
-	export CROSS_COMPILE_COMPAT="arm-eabi-"
-
-	export CC="aarch64-elf-gcc"
-	export LD="$GCC64_DIR/bin/aarch64-elf-ld"
-	export AR="aarch64-elf-gcc-ar"
-	export AS="aarch64-elf-as"
-	export NM="aarch64-elf-nm"
-	export OBJCOPY="aarch64-elf-objcopy"
-	export OBJDUMP="aarch64-elf-objdump"
-	export LLVM=0
-	export LLVM_IAS=0
-fi
-
 if [ "$clean" = true ]; then
 	rm -rf out
 	echo "Cleaned output folder"
@@ -107,9 +82,6 @@ fi
 
 echo -e "\nStarting compilation...\n"
 make O=out sweet_defconfig
-if [ "$gcc" = true ]; then
-	make O=out gcc-lto.config
-fi
 make -j$(nproc --all) O=out
 
 kernel="out/arch/arm64/boot/Image.gz"
@@ -133,7 +105,7 @@ else
 fi
 	
 sed -i "s/kernel\.string=.*/kernel.string=Spiteful Kernel by @vbajs on github/" AnyKernel3/anykernel.sh
-sed -i "s/supported\.versions=.*/supported.versions=11-16/" AnyKernel3/anykernel.sh
+sed -i "s/supported\.versions=.*/supported.versions=11-17/" AnyKernel3/anykernel.sh
 
 cp $kernel AnyKernel3
 cp $dtbo AnyKernel3
