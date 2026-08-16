dd58c781d368f9ebb74d62ac7207252353ed0240
Liang Hao <haohlliang@gmail.com>
2026-08-16 11:26:43 +0800

    sched/deadline: check start_dl_timer expiry with ktime_before()
    Replace ktime_us_delta() < 0 with ktime_before() so the past-expiry
    test uses the same resolution as act/now.
    
    Signed-off-by: Liang Hao <haohlliang@gmail.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Juri Lelli <juri.lelli@redhat.com>
    Link: https://patch.msgid.link/20260816032643.44969-1-haohlliang@gmail.com
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -1016,7 +1016,7 @@ static int start_dl_timer(struct task_struct *p)
 	 * chosen as the deadline is too small, don't even try to
 	 * start the timer in the past!
 	 */
-	if (ktime_us_delta(act, now) < 0)
+	if (ktime_before(act, now))
 		return 0;
 
 	/*
