e4626cc8b2a5579772d92d93a1d9cd0fb9c194fe
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-09-04 02:36:22 +0300

    Revert "sched/cass: respect uclamp max clamping"
    This reverts commit 7489bb1cce75ef832751e7f536e57e3bfcaacacb.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -76,7 +76,6 @@ void cass_cpu_util(struct cass_cpu_cand *c, int this_cpu, bool sync)
 static __always_inline
 bool cass_cpu_better(const struct cass_cpu_cand *a,
 		     const struct cass_cpu_cand *b, unsigned long p_util,
-		     unsigned long uc_max,
 		     int this_cpu, int prev_cpu, int prev_llc_id, bool sync)
 {
 #define cass_cmp(a, b) ({ res = (a) - (b); })
@@ -99,20 +98,6 @@ bool cass_cpu_better(const struct cass_cpu_cand *a,
 	if (cass_cmp(fits_capacity(p_util, a->cap_max),
 		     fits_capacity(p_util, b->cap_max)))
 		goto done;
-
-	/* Prefer smaller CPUs for tasks capped by uclamp_max below Prime */
-	if (uc_max < SCHED_CAPACITY_SCALE) {
-		bool a_fits = a->cap >= uc_max;
-		bool b_fits = b->cap >= uc_max;
-
-		/* Prefer a CPU that satisfies the cap over one that doesn't */
-		if (cass_cmp(a_fits, b_fits))
-			goto done;
-
-		/* Among CPUs that satisfy the cap, prefer the smaller one */
-		if (a_fits && b_fits && cass_cmp(b->cap, a->cap))
-			goto done;
-	}
 
 	/* Prefer the CPU with lower relative utilization */
 	if (cass_cmp(b->util, a->util))
@@ -181,7 +166,7 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 	/* Initialize @best such that @best always has a valid CPU at the end */
 	struct cass_cpu_cand cands[2], *best = cands;
 	int this_cpu = raw_smp_processor_id();
-	unsigned long p_util, uc_min, uc_max;
+	unsigned long p_util, uc_min;
 	bool has_idle = false;
 	int cidx = 0, cpu, prev_llc_id;
 
@@ -191,7 +176,6 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 	 */
 	p_util = rt ? 0 : task_util_est(p);
 	uc_min = uclamp_eff_value(p, UCLAMP_MIN);
-	uc_max = uclamp_eff_value(p, UCLAMP_MAX);
 
 	/*
 	 * When the LLC spans all CPUs (e.g. DynamIQ), every candidate shares
@@ -286,7 +270,7 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 		 * overloaded, since the relative utilization calculation
 		 * disregards thermal pressure.
 		 */
-		curr->eff_util = min(max(curr->util + curr->hard_util, uc_min), uc_max);
+		curr->eff_util = max(curr->util + curr->hard_util, uc_min);
 
 		/* Clamp the utilization to the minimum performance threshold */
 		if (curr->util < uc_min)
@@ -314,8 +298,8 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 		 * cidx still needs to be changed to the other candidate slot.
 		 */
 		if (best == curr ||
-		    cass_cpu_better(curr, best, p_util, uc_max, this_cpu,
-				    prev_cpu, prev_llc_id, sync)) {
+		    cass_cpu_better(curr, best, p_util, this_cpu, prev_cpu,
+				    prev_llc_id, sync)) {
 			best = curr;
 			cidx ^= 1;
 		}
