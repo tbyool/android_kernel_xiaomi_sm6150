fecd195a6f3809c77e593674adb5cc19a228d0ff
Yahya Wessam <yahyawessam2002@gmail.com>
2026-09-18 23:00:23 +0300

    Revert "mm/page_alloc: Fix DDR/CPU boost placing"
    This reverts commit 90c74607d47202eefa8c79f965d6880ff699e4b4.
    
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4211,6 +4211,8 @@ retry:
 	if (gfp_mask & __GFP_KSWAPD_RECLAIM)
 		wake_all_kswapds(order, ac);
 
+	/* Boost when memory is low so allocation latency doesn't get too bad */
+	devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 100);
 
 	reserve_flags = __gfp_pfmemalloc_flags(gfp_mask);
 	if (reserve_flags)
@@ -4281,9 +4283,6 @@ retry:
 	if (costly_order && (!can_compact ||
 			     !(gfp_mask & __GFP_RETRY_MAYFAIL)))
 		goto nopage;
-
-	/* Boost when memory is low so allocation latency doesn't get too bad */
-	devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 100);
 
 	if (should_reclaim_retry(gfp_mask, order, ac, alloc_flags,
 				 did_some_progress > 0, &no_progress_loops))
